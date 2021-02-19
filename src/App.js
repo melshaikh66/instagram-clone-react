f8fed40bf76babcb9c65f807e96e23a7bd2cf34b
Mostafa Elshaikh <m.elshaikh@gmail.com>
2021-02-19 13:43:39 +0200

    mm new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello world from git</h1>
+      <h1>Hello I will make instagram clone</h1>
     </div>
   );
 }
